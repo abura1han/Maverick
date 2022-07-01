d007471d9be31aef2059b4196958f4055bd14e5e
abura1han <xbura1han@gmail.com>
2022-07-01 12:23:46 +0600

    Update: App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import { useState } from 'react'
+import GlobalSettings from './context/GlobalSettings'
+import Wrapper from './components/Wrapper'
 
 const App = () => {
   // Global theme config
@@ -10,65 +14,91 @@ const App = () => {
     },
   })
 
+  const userDefaultSettings = {
+    isNotificationDialogOpen: false,
+  }
+
+  const [globalSettings, setGlobalSettings] = useState(userDefaultSettings)
+
   return (
     <ThemeProvider theme={THEME}>
-      <BrowserRouter>
-        <Routes>
-          {/* People */}
-          <Route path="/people">
-            <Route index element={<>People page</>} />
-          </Route>
+      <GlobalSettings.Provider value={{ globalSettings, setGlobalSettings }}>
+        <BrowserRouter>
+          <Routes>
+            {/* People */}
+            <Route path="/people">
+              <Route index element={<>People page</>} />
+            </Route>
 
-          {/* Skills */}
-          <Route path="/skills">
-            <Route index element={<>Skills page</>} />
-          </Route>
+            {/* Skills */}
+            <Route path="/skills">
+              <Route index element={<>Skills page</>} />
+            </Route>
 
-          {/* Challenges */}
-          <Route path="/challenges">
-            <Route index element={<>Challenges page</>} />
-          </Route>
+            {/* Challenges */}
+            <Route path="/challenges">
+              <Route index element={<>Challenges page</>} />
+            </Route>
 
-          {/* Top */}
-          <Route path="/top">
-            <Route index element={<>Top page</>} />
-          </Route>
+            {/* Top */}
+            <Route path="/top">
+              <Route index element={<>Top page</>} />
+            </Route>
 
-          {/* Post */}
-          <Route path="/m/:postId">
-            <Route index element={<>Post page</>} />
-          </Route>
+            {/* Post */}
+            <Route path="/m/:postId">
+              <Route index element={<>Post page</>} />
+            </Route>
 
-          {/* Profile */}
-          <Route path="/u/:userName">
-            <Route index element={<>Profile page</>} />
-            <Route path="saved" element={<>saved page</>} />
-            <Route path="settings" element={<>Settings page</>} />
-          </Route>
+            {/* Profile */}
+            <Route path="/u/:userName">
+              <Route index element={<>Profile page</>} />
+              <Route path="saved" element={<>saved page</>} />
+              <Route path="settings" element={<>Settings page</>} />
+            </Route>
 
-          {/* Search */}
-          <Route path="/search">
-            <Route index element={<>Search page</>} />
-          </Route>
+            {/* Search */}
+            <Route path="/search">
+              <Route
+                index
+                element={
+                  <>
+                    {' '}
+                    <>
+                      <Header />
+                    </>
+                  </>
+                }
+              />
+            </Route>
 
-          {/* onboard */}
-          <Route path="/onboard">
-            <Route index element={<>Login| Signup page</>} />
-            <Route
-              path="setup-account"
-              element={<>Setup account page onboard</>}
-            />
-          </Route>
+            {/* onboard */}
+            <Route path="/onboard">
+              <Route index element={<>Login| Signup page</>} />
+              <Route
+                path="setup-account"
+                element={<>Setup account page onboard</>}
+              />
+            </Route>
 
-          {/* Home page */}
-          <Route path="/">
-            <Route index element={<>Index page</>} />
-          </Route>
+            {/* Home page */}
+            <Route path="/">
+              <Route
+                index
+                element={
+                  <>
+                    <Header />
+                    <Wrapper />
+                  </>
+                }
+              />
+            </Route>
 
-          {/* 404 page */}
-          <Route path="*" element={<>404 page not found</>} />
-        </Routes>
-      </BrowserRouter>
+            {/* 404 page */}
+            <Route path="*" element={<>404 page not found</>} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalSettings.Provider>
     </ThemeProvider>
   )
 }
